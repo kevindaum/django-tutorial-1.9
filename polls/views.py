746ee8d53b7f70a49255b1aebc8cc704ccf46aa5
Kevin Daum <daum1kc@cmich.edu>
2016-07-05 16:04:23 -0400

    Step 2.1
    The state just before
    [making the views actually do something](https://docs.djangoproject.com/en/1.9/intro/tutorial03/#write-views-that-actually-do-something).

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Greetings humans! You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question {question}.".format(question=question_id))
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question {question}."
+    return HttpResponse(response.format(question=question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)  # aren't we supposed to be using .format in python 3?
